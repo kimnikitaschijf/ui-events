b44e4d3c0382c7df5d4d6ea81226bbd7b3449219
kimnikitaschijf <kimnikitaschijfhva@icloud.com>
2024-12-11 11:43:07 +0100

    Design knop af

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,20 +7,27 @@ function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 
+// MIJN JAVA SCRIPT:
 
-
-
-
-
-
-
-
+// frontend knop
 let frontend = document.querySelector('a:nth-of-type(1)')
 
-let zoomIn = function() {
+let zoomIn = function () {
   frontend.classList.toggle('zoom-in');
 }
-frontend.addEventListener('click',zoomIn)
+frontend.addEventListener('click', zoomIn)
+
+
+// design knop
+let design = document.querySelector('a:nth-of-type(2)')
+
+  design.addEventListener('mouseover', () => {
+    design.classList.add('flip');
+  });
+
+  design.addEventListener('mouseleave', () => {
+    design.classList.remove('flip');
+  });
 
 
 
