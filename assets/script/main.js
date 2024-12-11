a002ac685ba8fe6663899af1ae478f8b87da7d2f
kimnikitaschijf <kimnikitaschijfhva@icloud.com>
2024-12-11 11:10:33 +0100

    Frontend button af

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,21 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+
+
+
+
+
+
+
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+let zoomIn = function() {
+  frontend.classList.toggle('zoom-in');
+}
+frontend.addEventListener('click',zoomIn)
+
+
+
